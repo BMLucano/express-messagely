ad69d2e8498711a2356e9401b5a22ae2bddbb83e
Brandie Lucano <blucano.dev@gmail.com>
2024-01-08 16:53:03 -0800

    imported user class and secret key. Completed login route

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,31 @@
 
 const Router = require("express").Router;
 const router = new Router();
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+const SECRET_KEY = require("./config");
+const { BadRequestError } = require("../expressError");
 
 /** POST /login: {username, password} => {token} */
 
+router.post("/login", async function(req, res, next){
+  //check for body
+  //get user name and password from body
+  //call User.authenticate with given username/password
+    //if true, sign token and return it
+  if (req.body === undefined) throw new BadRequestError();
+  const { username, password } = req.body;
+
+  if(await User.authenticate(username, password)){
+    const token = jwt.sign({ username }, SECRET_KEY);
+    User.updateLoginTimestamp(username);
+    return res.json({ token });
+  }
+
+  throw new UnauthorizedError("Invalid user/password");
+
+})
 
 /** POST /register: registers, logs in, and returns token.
  *
